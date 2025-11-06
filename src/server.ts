4e455bb36c19a88501335fc21271166abaef17f1
michael-gokey-architect <michael.gokey.architect@gmail.com>
2025-11-05 23:47:40 -0500

    Step Next, Troubleshooting Angular Package Version Conflicts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // src/server.ts
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './main.server';
